fea0e900b40ed22853b123a0b3fad3777a121b9a
joshkim2441 <joshkim2441@gmail.com>
2024-03-22 23:59:41 +0300

    Task 1

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,5 @@
 # Install especific version of flask (2.1.0)
 
-exec {'puppet-lint':
-  command => '/usr/bin/sudo apt-get -y install puppet-lint -v 2.1.0',
+exec {'install-flask':
+  command => '/usr/bin/sudo pip3 install flask==2.1.0',
 }
