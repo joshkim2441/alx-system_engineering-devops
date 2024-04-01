1d44ccc04637e9e04cad0151654bf41352ed742b
joshkim2441 <joshkim2441@gmail.com>
2024-04-01 15:55:23 +0300

    Web server

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,81 +1,44 @@
 # Add a stable version of Nginx
 
-exec { 'add nginx stable repo':
-  command => 'sudo add-apt-repository ppa:nginx/stable',
-  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
+# 7-puppet_install_nginx_web_server.pp
+exec { 'update':
+  command => 'apt-get update -y',
+  path    => ['/usr/bin', '/usr/sbin', '/bin'],
 }
 
-# Update software packages list
-exec { 'update packages':
-  command => 'sudo apt-get update -y',
-  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
-}
-
-# Install Nginx
 package { 'nginx':
   ensure  => 'installed',
+  require => Exec['update'],
 }
 
-# Allow HTTP
-exec { 'allow HTTP':
-  command => "ufw allow 'Nginx HTTP'",
-  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
-  onlyif  => '! dpkg -l nginx | egrep \'ii.*nginx\' > /dev/null 2>$1',
-}
-
-# Change folder rights
-exec { 'chmod www folder':
-  command => 'chmod -R 755 /var/www',
-  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
-}
-
-# Create an index file
 file { '/var/www/html/index.html':
-  content => "Hello World!\n",
+  ensure  => 'file',
+  content => 'Hello World!\n',
+  require => Package['nginx'],
 }
 
-# Create an index file
-file { '/var/www/html/404.html':
-  content => "Ceci n'est pas une page\n",
-}
-
-# Add a redirection and error page
-file { 'Nginx default config file':
-  ensure  => file,
-  path    => '/etc/nginx/sites-enabled/default',
-  content =>
-"server {
-        listen 80 default_server;
-        listen [::]:80 default_server;
-               root /var/www/html;
-        # Add index.php to the list if you are using PHP
-        index index.html index.htm index.nginx-debian.html;
-        server_name _;
-        location / {
-                # First attempt to serve request as file, then
-                # as directory, then fall back to displaying a 404.
-                try_files \$uri \$uri/ =404;
-        }
-        error_page 404 /404.html;
-        location  /404.html {
-            internal;
-        }
-
-        if (\$request_filename ~ redirect_me){
-            rewrite ^ https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;
-        }
+file { '/etc/nginx/sites-available/default':
+  ensure  => 'file',
+  content => "
+server {
+  listen 80 default_server;
+  listen [::]:80 default_server;
+  root /var/www/html;
+  index index.html index.htm index.nginx-debian.html;
+  server_name _;
+  location / {
+    try_files \$uri \$uri/ =404;
+  }
+  location /redirect_me {
+    return 301 https://www.youtube.com/watch?v=QH2-TGUlwu4;
+  }
 }
 ",
+  require => Package['nginx'],
 }
 
-# Restart the Nginx service
-exec { 'restart service':
-  command => 'service nginx restart',
-  path    => '/usr/bin:/usr/sbin:/bin',
-}
-
-# Start the Nginx service
 service { 'nginx':
-  ensure   => running,
-  require  => Package['nginx'],
+  ensure    => 'running',
+  enable    => true,
+  subscribe => File['/etc/nginx/sites-available/default'],
 }
