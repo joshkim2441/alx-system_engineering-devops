6f8fdb400e1da8394c2b333a9b9f73016a37a935
joshkim2441 <joshkim2441@gmail.com>
2024-04-22 16:14:14 +0300

    API

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Use a REST API to fetch info about a given employee's ID"""
+"""Fetch all tasks from all employees and export in json format"""
 import json
 import requests
 
@@ -8,20 +8,34 @@ base_url = 'https://jsonplaceholder.typicode.com'
 if __name__ == '__main__':
 
     users_url = '{}/users'.format(base_url)
+    # get info from api
     response = requests.get(users_url)
+    # pull data from api
     data = response.text
+    # parse the data into json format
     data = json.loads(data)
 
+    # extract users data
     builder = {}
     for user in data:
+        # print("id is: {}".format(user_id))
         user_id = user.get('id')
+        # print("username is: {}".format(user_name))
         user_name = user.get('username')
+        # print("dict_key: {}".format(dict_key))
         dict_key = str(user_id)
+        # get user info about todo tasks
+        # ex: https://jsonplaceholder.typicode.com/users/1/todos
         builder[dict_key] = []
+        # print("tasks url is: {}".format(tasks_url))
         tasks_url = '{}/todos?userId={}'.format(base_url, user_id)
 
+        # get info from api
         response = requests.get(tasks_url)
+        # pull data from api
         tasks = response.text
+        # parse the data into JSON format
+        # prrint("JSON LOADS IS: {}".format(tasks))
         tasks = json.loads(tasks)
 
         for task in tasks:
@@ -30,7 +44,9 @@ if __name__ == '__main__':
                 "completed": task['completed'],
                 "username": user_name
             }
+            # append dictionary key to the dictionary
             builder[dict_key].append(json_data)
+    # write the data to the file
     json_encoded_data = json.dumps(builder)
     with open('todo_all_employees.json', 'w', encoding='UTF8') as myFile:
         myFile.write(json_encoded_data)
