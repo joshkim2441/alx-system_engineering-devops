9589c93657cfcf772797385d1f555c5a1ed5f926
joshkim2441 <joshkim2441@gmail.com>
2024-05-07 14:07:32 +0300

    Task 0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -8,12 +8,12 @@ def number_of_subscribers(subreddit):
     """
     import requests
 
-    headers = {"User-Agent": "My-User-Agent"}
+    headers = {"User-Agent": "Mozilla/5.0"}
     response = requests.get("https://www.reddit.com/r/{}/about.json".
                             format(subreddit), headers=headers,
                             allow_redirects=False)
 
-    if response.status_code != 200:
+    if response.status_code >= 300:
         return 0
 
     return response.json().get("data").get("subscribers")
