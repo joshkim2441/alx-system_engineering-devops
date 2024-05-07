c743efa4fb790539b6d5a8fc5d5a21beec5a1c6a
joshkim2441 <joshkim2441@gmail.com>
2024-05-07 22:38:35 +0300

    Advanced API

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -4,7 +4,8 @@
 
 def number_of_subscribers(subreddit):
     """Query the Reddit API and returns the number of subscribers
-    to the subreddit
+    to the subreddit. Checks for bugs such as null pointer references,
+    unhandled exceptions, and more.
     """
     import requests
 
@@ -13,6 +14,15 @@ def number_of_subscribers(subreddit):
 
     headers = {"User-Agent": "Mozilla/5.0"}
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    response = requests.get(url, headers=headers, allow_redirects=False)
-
-    return response.json().get("data").get("subscribers")
+    try:
+        response = requests.get(url, headers=headers, allow_redirects=False)
+        if response.status_code != 200:
+            raise ValueError('Non-200 status code')
+        data = response.json()
+        if data is None or 'data' not in data:
+            raise ValueError('Invalid response from server')
+        return data['data'].get('subscribers')
+    except AttributeError as e:
+        raise TypeError('Missing attribute in response') from e
+    except (TypeError, KeyError, ValueError) as e:
+        raise ValueError('Invalid response from server') from e
