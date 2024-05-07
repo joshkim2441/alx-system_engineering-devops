cd0d38eb039e0aa8ca085d2a4ae3fb1f7fa7565f
joshkim2441 <joshkim2441@gmail.com>
2024-05-07 20:01:07 +0300

    Advanced API

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -8,11 +8,11 @@ def number_of_subscribers(subreddit):
     """
     import requests
 
+    if subreddit is None or not isinstance(subreddit, str):
+        return 0
 
     headers = {"User-Agent": "Mozilla/5.0"}
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code == 404:
-        return 0
-    res = response.json().get("data")
-    return res.get("subscribers")
+
+    return response.json().get("data").get("subscribers")
