b9d7240d50f00c5ec4508e1994fa2fd066c34cf5
joshkim2441 <joshkim2441@gmail.com>
2024-05-07 14:27:50 +0300

    Advanced API

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -13,6 +13,7 @@ def top_ten(subreddit):
     response = requests.get("https://www.reddit.com/r/{}/hot.json?limit=10".
                             format(subreddit),
                             headers=headers, allow_redirects=False)
+
     if response.status_code >= 300:
         print('None')
     else:
