ef5f5eff47f05045e362d547b2589081718976c6
joshkim2441 <joshkim2441@gmail.com>
2024-05-07 15:03:01 +0300

    Advanced API

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Print the titles of the first 10 hot posts listed in the subreddit
+"""
+
+
+def top_ten(subreddit):
+    """Print the titles of the first 10 hot
+    posts listed in the subreddit
+    """
+    import requests
+
+    if subreddit is None or not isinstance(subreddit, str):
+        print("None")
+
+    headers = {"User-Agent": "Mozilla/5.0"}
+    url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
+    response = requests.get(url, headers=headers)
+
+    if response.status_code != 200:
+        print('None')
+    else:
+        [print(post.get("data").get("title"))
+         for post in response.json().get("data").get("children")]
