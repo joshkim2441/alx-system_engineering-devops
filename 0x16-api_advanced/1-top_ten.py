f2a56a07c5b739899de5d6d2f3f1b66e2b7e5eb2
joshkim2441 <joshkim2441@gmail.com>
2024-05-07 15:44:48 +0300

    Advanced API

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,23 +1,29 @@
 #!/usr/bin/python3
 """Print the titles of the first 10 hot posts listed in the subreddit
 """
+from requests import get
 
 
 def top_ten(subreddit):
     """Print the titles of the first 10 hot
     posts listed in the subreddit
     """
-    import requests
 
     if subreddit is None or not isinstance(subreddit, str):
         print("None")
 
-    headers = {"User-Agent": "Mozilla/5.0"}
-    url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
-    response = requests.get(url, headers=headers)
+    user_agent = {'User-agent': 'Google Chrome Version 81.0.4044.129'}
+    params = {'limit': 10}
+    url = 'https://www.reddit.com/r/{}/hot/.json'.format(subreddit)
 
-    if response.status_code != 200:
-        print('None')
-    else:
-        [print(post.get("data").get("title"))
-         for post in response.json().get("data").get("children")]
+    response = get(url, headers=user_agent, params=params)
+    results = response.json()
+
+    try:
+        my_data = results.get('data').get('children')
+
+        for i in my_data:
+            print(i.get('data').get('title'))
+
+    except Exception:
+        print("None")
