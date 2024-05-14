07830ab995db2e8ed907bd814bda15e583b34082
joshkim2441 <joshkim2441@gmail.com>
2024-05-14 18:57:19 +0300

    web stack debug

---
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -0,0 +1,7 @@
+# Change the OS configuration to enable login using
+# the holberton user and open a file without any error message.
+
+exec {'OS security config':
+  command => 'sed -i "s/holberton/foo/" /etc/security/limits.conf',
+  path    => '/usr/bin/env/:/bin/:/usr/bin/:/usr/sbin/'
+}
